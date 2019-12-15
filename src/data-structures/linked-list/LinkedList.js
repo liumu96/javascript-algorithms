2d6029500f889346b1f2248f999f88805b1b5808
liumu96 <918883717@qq.com>
2019-12-15 12:04:19 +0800

    双向链表

--- src/data-structures/linked-list/LinkedList.js
+++ src/data-structures/linked-list/LinkedList.js
@@ -1,172 +1,172 @@
 import LinkedListNode from './LinkedListNode';
-import Comparator from '../../utils/Comparator';
+import Comparator from '../../utils/comparator/Comparator';
 
 export default class LinkedList {
-    constructor(comparatorFunction) {
-        this.head = null;
-        this.tail = null;
-        this.compare = new Comparator(comparatorFunction);
+  constructor(comparatorFunction) {
+    this.head = null;
+    this.tail = null;
+    this.compare = new Comparator(comparatorFunction);
+  }
+
+  prepend(value) {
+    const newNode = new LinkedListNode(value, this.head);
+    this.head = newNode;
+
+    if (!this.tail) {
+      this.tail = newNode;
+    }
+    return this;
+  }
+
+  append(value) {
+    const newNode = new LinkedListNode(value);
+
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+
+      return this;
     }
 
-    prepend(value) {
-        const newNode = new LinkedListNode(value, this.head);
-        this.head = newNode;
+    this.tail.next = newNode;
+    this.tail = newNode;
 
-        if(!this.tail) {
-            this.tail = newNode;
-        }
-        return this;
+    return this;
+  }
+
+  delete(value) {
+    if (!this.head) {
+      return null;
     }
 
-    append(value) {
-        const newNode = new LinkedListNode(value);
+    let deleteNode = null;
 
-        if(!this.head) {
-            this.head = newNode;
-            this.tail = newNode;
-
-            return this;
-        }
-
-        this.tail.next = newNode;
-        this.tail = newNode;
-
-        return this;
+    while (this.head && this.compare.equal(this.head.value, value)) {
+      deleteNode = this.head;
+      this.head = this.head.next;
     }
 
-    delete(value) {
-        if(!this.head) {
-            return null;
-        }
+    let currentNode = this.head;
 
-        let deleteNode = null;
-
-        while(this.head && this.compare.equal(this.head.value, value)) {
-            deleteNode = this.head;
-            this.head = this.head.next;
-        }
-
-        let currentNode = this.head;
-
-        if(currentNode !== null) {
-            while (currentNode.next) {
-                if (this.compare.equal(currentNode.next.value, value)) {
-                    deleteNode = currentNode.next;
-                    currentNode.next = currentNode.next.next;
-                } else {
-                    currentNode = currentNode.next;
-                }
-            }
-        }
-
-        if (this.compare.equal(this.tail.value, value)) {
-            this.tail = currentNode;
-        }
-
-        return deleteNode;
-    }
-
-    find(value = undefined, callback = undefined) {
-        if (!this.head) {
-            return null;
-        }
-
-        let currentNode = this.head;
-
-        while(currentNode) {
-            if (callback && callback(currentNode.value)) {
-                return currentNode;
-            }
-            if (value !== undefined && this.compare.equal(currentNode.value, value)) {
-                return currentNode;
-            }
-            
-            currentNode = currentNode.next;
-        }
-
-        return null;
-    }
-
-    deleteTail() {
-        const deletedTail = this.tail;
-
-        if(this.head === this.tail) {
-            this.head = null;
-            this.tail = null;
-
-            return deletedTail;
-        }
-
-        let currentNode = this.head;
-        while (currentNode.next) {
-            if (!currentNode.next.next) {
-                currentNode.next = null;
-            } else {
-                currentNode = currentNode.next;
-            }
-        }
-
-        this.tail = currentNode;
-        
-        return deletedTail;
-    }
-
-    deleteHead() {
-        if (!this.head) {
-            return null;
-        }
-
-        const deletedHead = this.head;
-
-        if (this.head.next) {
-            this.head = this.head.next;
+    if (currentNode !== null) {
+      while (currentNode.next) {
+        if (this.compare.equal(currentNode.next.value, value)) {
+          deleteNode = currentNode.next;
+          currentNode.next = currentNode.next.next;
         } else {
-            this.head = null;
-            this.tail = null;
+          currentNode = currentNode.next;
         }
-
-        return deletedHead;
+      }
     }
 
-    fromArray(values) {
-        values.forEach(value => this.append(value));
-
-        return this;
+    if (this.compare.equal(this.tail.value, value)) {
+      this.tail = currentNode;
     }
 
-    toArray() {
-        const nodes = [];
+    return deleteNode;
+  }
 
-        let currentNode = this.head;
-        while (currentNode) {
-            nodes.push(currentNode);
-            currentNode = currentNode.next;
-        }
-
-        return nodes;
+  find(value = undefined, callback = undefined) {
+    if (!this.head) {
+      return null;
     }
 
-    toString(callback) {
-        return this.toArray().map(node => node.toString(callback).toString());
+    let currentNode = this.head;
+
+    while (currentNode) {
+      if (callback && callback(currentNode.value)) {
+        return currentNode;
+      }
+      if (value !== undefined && this.compare.equal(currentNode.value, value)) {
+        return currentNode;
+      }
+
+      currentNode = currentNode.next;
     }
 
-    reverse() {
-        let currNode = this.head;
-        let prevNode = null;
-        let nextNode = null;
+    return null;
+  }
 
-        while (currNode) {
-            nextNode = currNode.next;
+  deleteTail() {
+    const deletedTail = this.tail;
 
-            currNode.next = prevNode;
+    if (this.head === this.tail) {
+      this.head = null;
+      this.tail = null;
 
-            prevNode = currNode;
-            currNode = nextNode;
-        }
-
-        this.tail = this.head;
-        this.head = prevNode;
-
-        return this;
+      return deletedTail;
     }
+
+    let currentNode = this.head;
+    while (currentNode.next) {
+      if (!currentNode.next.next) {
+        currentNode.next = null;
+      } else {
+        currentNode = currentNode.next;
+      }
+    }
+
+    this.tail = currentNode;
+
+    return deletedTail;
+  }
+
+  deleteHead() {
+    if (!this.head) {
+      return null;
+    }
+
+    const deletedHead = this.head;
+
+    if (this.head.next) {
+      this.head = this.head.next;
+    } else {
+      this.head = null;
+      this.tail = null;
+    }
+
+    return deletedHead;
+  }
+
+  fromArray(values) {
+    values.forEach((value) => this.append(value));
+
+    return this;
+  }
+
+  toArray() {
+    const nodes = [];
+
+    let currentNode = this.head;
+    while (currentNode) {
+      nodes.push(currentNode);
+      currentNode = currentNode.next;
+    }
+
+    return nodes;
+  }
+
+  toString(callback) {
+    return this.toArray().map((node) => node.toString(callback)).toString();
+  }
+
+  reverse() {
+    let currNode = this.head;
+    let prevNode = null;
+    let nextNode = null;
+
+    while (currNode) {
+      nextNode = currNode.next;
+
+      currNode.next = prevNode;
+
+      prevNode = currNode;
+      currNode = nextNode;
+    }
+
+    this.tail = this.head;
+    this.head = prevNode;
+
+    return this;
+  }
 }
